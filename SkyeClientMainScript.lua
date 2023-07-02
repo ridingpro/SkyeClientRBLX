2885b6f16c9604ac836ab262cc67a38247de91d7
Exploit User <132393158+ridingpro@users.noreply.github.com>
2023-07-02 16:45:11 +0100

    massive update

--- SkyeClientMainScript.lua
+++ SkyeClientMainScript.lua
@@ -1,1 +1,310 @@
--- coming back...
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Skye Client | Public | BETA", "GrapeTheme")
+local Tab = Window:NewTab("Main")
+local Tab2 = Window:NewTab("Credits")
+local Section = Tab:NewSection("Combat")
+local Section2 = Tab2:NewSection("Credits")
+Section2:NewLabel("Credits")
+Section2:NewLabel("Code: 7GrandDadPGN with Vape V4")
+Section:NewLabel("Combat")
+Section:NewToggle("Reach", "Extends sword reach", function(state)
+    if state then
+        bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = ReachValue.Value + 3.3
+			else
+				bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = 14.4
+			end
+end)
+Section:NewToggle("AutoSprint", "Don't hold that annoying rightshift key to sprint.", function(callback)
+    if callback then
+if inputService.TouchEnabled then
+					pcall(function() lplr.PlayerGui.MobileUI["2"].Visible = false end)
+				end
+				oldSprintFunction = bedwars.SprintController.stopSprinting
+				bedwars.SprintController.stopSprinting = function(...)
+					local originalCall = oldSprintFunction(...)
+					bedwars.SprintController:startSprinting()
+					return originalCall
+				end
+				table.insert(Sprint.Connections, lplr.CharacterAdded:Connect(function(char)
+					char:WaitForChild("Humanoid", 9e9)
+					task.wait(0.5)
+					bedwars.SprintController:stopSprinting()
+				end))
+				task.spawn(function()
+					bedwars.SprintController:startSprinting()
+				end)
+			else
+				if inputService.TouchEnabled then
+					pcall(function() lplr.PlayerGui.MobileUI["2"].Visible = true end)
+				end
+				bedwars.SprintController.stopSprinting = oldSprintFunction
+				bedwars.SprintController:stopSprinting()
+			end
+end)
+Section:NewToggle("Velocity", "Reduces knockback taken.", function(callback)
+    if callback then
+       applyKnockback = bedwars.KnockbackUtil.applyKnockback
+				bedwars.KnockbackUtil.applyKnockback = function(root, mass, dir, knockback, ...)
+					knockback = knockback or {}
+					if VelocityHorizontal.Value == 0 and VelocityVertical.Value == 0 then return end
+					knockback.horizontal = (knockback.horizontal or 1) * (VelocityHorizontal.Value / 100)
+					knockback.vertical = (knockback.vertical or 1) * (VelocityVertical.Value / 100)
+					return applyKnockback(root, mass, dir, knockback, ...)
+				end
+			else
+				bedwars.KnockbackUtil.applyKnockback = applyKnockback
+			end
+end)
+Section:NewLabel("Blatant")
+runFunction(function()
+	local AutoLeaveDelay = {Value = 1}
+	local AutoPlayAgain = {Enabled = false}
+	local AutoLeaveStaff = {Enabled = true}
+	local AutoLeaveStaff2 = {Enabled = true}
+	local AutoLeaveRandom = {Enabled = false}
+	local leaveAttempted = false
+
+	local function getRole(plr)
+		local suc, res = pcall(function() return plr:GetRankInGroup(5774246) end)
+		if not suc then 
+			repeat
+				suc, res = pcall(function() return plr:GetRankInGroup(5774246) end)
+				task.wait()
+			until suc
+		end
+		if plr.UserId == 1774814725 then 
+			return 200
+		end
+		return res
+	end
+
+	local flyAllowedmodules = {"Sprint", "AutoClicker", "AutoReport", "AutoReportV2", "AutoRelic", "AimAssist", "AutoLeave", "Reach"}
+	local function autoLeaveAdded(plr)
+		task.spawn(function()
+			if not shared.VapeFullyLoaded then
+				repeat task.wait() until shared.VapeFullyLoaded
+			end
+			if getRole(plr) >= 100 then
+				if AutoLeaveStaff.Enabled then
+					if #bedwars.ClientStoreHandler:getState().Party.members > 0 then 
+						bedwars.LobbyClientEvents.leaveParty()
+					end
+					if AutoLeaveStaff2.Enabled then 
+						GuiLibrary.SaveSettings = function() end
+						for i,v in pairs(GuiLibrary.ObjectsThatCanBeSaved) do 
+							if v.Type == "OptionsButton" then
+								if table.find(flyAllowedmodules, i:gsub("OptionsButton", "")) == nil and tostring(v.Object.Parent.Parent):find("Render") == nil then
+									if v.Api.Enabled then
+										v.Api.ToggleButton(false)
+									end
+									v.Api.SetKeybind("")
+									v.Object.TextButton.Visible = false
+								end
+							end
+						end
+					else
+                Section:NewToggle("AutoLeave", "Queues on it's own", function(callback)
+    if callback then
+        table.insert(AutoLeave.Connections, vapeEvents.EntityDeathEvent.Event:Connect(function(deathTable)
+					if (not leaveAttempted) and deathTable.finalKill and deathTable.entityInstance == lplr.Character then
+						leaveAttempted = true
+						if isEveryoneDead() and bedwarsStore.matchState ~= 2 then
+							task.wait(1 + (AutoLeaveDelay.Value / 10))
+							if bedwars.ClientStoreHandler:getState().Game.customMatch == nil and bedwars.ClientStoreHandler:getState().Party.leader.userId == lplr.UserId then
+								if not AutoPlayAgain.Enabled then
+									bedwars.ClientHandler:Get("TeleportToLobby"):SendToServer()
+								else
+									if AutoLeaveRandom.Enabled then 
+										local listofmodes = {}
+										for i,v in pairs(bedwars.QueueMeta) do
+											if not v.disabled and not v.voiceChatOnly and not v.rankCategory then table.insert(listofmodes, i) end
+										end
+										bedwars.LobbyClientEvents:joinQueue(listofmodes[math.random(1, #listofmodes)])
+									else
+										bedwars.LobbyClientEvents:joinQueue(bedwarsStore.queueType)
+									end
+								end
+							end
+						end
+					end
+				end))
+				table.insert(AutoLeave.Connections, vapeEvents.MatchEndEvent.Event:Connect(function(deathTable)
+					task.wait(AutoLeaveDelay.Value / 10)
+					if not AutoLeave.Enabled then return end
+					if leaveAttempted then return end
+					leaveAttempted = true
+					if bedwars.ClientStoreHandler:getState().Game.customMatch == nil and bedwars.ClientStoreHandler:getState().Party.leader.userId == lplr.UserId then
+						if not AutoPlayAgain.Enabled then
+							bedwars.ClientHandler:Get("TeleportToLobby"):SendToServer()
+						else
+							if bedwars.ClientStoreHandler:getState().Party.queueState == 0 then
+								if AutoLeaveRandom.Enabled then 
+									local listofmodes = {}
+									for i,v in pairs(bedwars.QueueMeta) do
+										if not v.disabled and not v.voiceChatOnly and not v.rankCategory then table.insert(listofmodes, i) end
+									end
+									bedwars.LobbyClientEvents:joinQueue(listofmodes[math.random(1, #listofmodes)])
+								else
+									bedwars.LobbyClientEvents:joinQueue(bedwarsStore.queueType)
+								end
+							end
+						end
+					end
+				end))
+				table.insert(AutoLeave.Connections, playersService.PlayerAdded:Connect(autoLeaveAdded))
+				for i, plr in pairs(playersService:GetPlayers()) do
+					autoLeaveAdded(plr)
+				end
+			end
+		end
+end)
+
+Section:NewToggle("GrapplingHookDisabler", "ToggleInfo", function(callback)
+    if callback then
+  		local grappleHooked = false
+				table.insert(GrappleExploit.Connections, bedwars.ClientHandler:Get("GrapplingHookFunctions"):Connect(function(p4)
+					if p4.hookFunction == "PLAYER_IN_TRANSIT" then
+						bedwarsStore.grapple = tick() + 1.8
+						grappleHooked = true
+						GrappleExploit.ToggleButton(false)
+					end
+				end))
+
+				local fireball = getItem("grappling_hook")
+				if fireball then 
+					task.spawn(function()
+						repeat task.wait() until bedwars.CooldownController:getRemainingCooldown("grappling_hook") == 0 or (not GrappleExploit.Enabled)
+						if (not GrappleExploit.Enabled) then return end
+						switchItem(fireball.tool)
+						local pos = entityLibrary.character.HumanoidRootPart.CFrame.p
+						local offsetshootpos = (CFrame.new(pos, pos + Vector3.new(0, -60, 0)) * CFrame.new(Vector3.new(-bedwars.BowConstantsTable.RelX, -bedwars.BowConstantsTable.RelY, -bedwars.BowConstantsTable.RelZ))).p
+						projectileRemote:CallServerAsync(fireball["tool"], nil, "grappling_hook_projectile", offsetshootpos, pos, Vector3.new(0, -60, 0), game:GetService("HttpService"):GenerateGUID(true), {drawDurationSeconds = 1}, workspace:GetServerTimeNow() - 0.045)
+					end)
+				else
+					GrappleExploit.ToggleButton(false)
+					return
+				end
+
+				local startCFrame = entityLibrary.isAlive and entityLibrary.character.HumanoidRootPart.CFrame
+				RunLoops:BindToHeartbeat("GrappleExploit", function(delta) 
+					if GuiLibrary.ObjectsThatCanBeSaved["Lobby CheckToggle"].Api.Enabled then 
+						if bedwars.matchState == 0 then return end
+					end
+					if entityLibrary.isAlive then
+						entityLibrary.character.HumanoidRootPart.Velocity = Vector3.zero
+						entityLibrary.character.HumanoidRootPart.CFrame = startCFrame
+					end
+				end)
+			else
+				GrappleExploitUp = false
+				GrappleExploitDown = false
+				RunLoops:UnbindFromHeartbeat("GrappleExploit")
+			end
+		end
+end)
+Section:NewToggle("LongJump", "basically fly but faster", function(callback)
+    if callback then
+        table.insert(LongJump.Connections, vapeEvents.EntityDamageEvent.Event:Connect(function(damageTable)
+					if damageTable.entityInstance == lplr.Character and (not damageTable.knockbackMultiplier or not damageTable.knockbackMultiplier.disabled) then 
+						local knockbackBoost = damageTable.knockbackMultiplier and damageTable.knockbackMultiplier.horizontal and damageTable.knockbackMultiplier.horizontal * LongJumpSpeed.Value or LongJumpSpeed.Value
+						if damagetimertick < tick() or knockbackBoost >= damagetimer then
+							damagetimer = knockbackBoost
+							damagetimertick = tick() + 2.5
+							local newDirection = entityLibrary.character.HumanoidRootPart.CFrame.lookVector
+							directionvec = Vector3.new(newDirection.X, 0, newDirection.Z).Unit
+						end
+					end
+				end))
+				task.spawn(function()
+					task.spawn(function()
+						repeat
+							task.wait()
+							if LongJumpacprogressbarframe then
+								LongJumpacprogressbarframe.BackgroundColor3 = Color3.fromHSV(GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Hue, GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Sat, GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Value)
+								LongJumpacprogressbarframe2.BackgroundColor3 = Color3.fromHSV(GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Hue, GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Sat, GuiLibrary.ObjectsThatCanBeSaved["Gui ColorSliderColor"].Api.Value)
+							end
+						until (not LongJump.Enabled)
+					end)
+					local LongJumpOrigin = entityLibrary.isAlive and entityLibrary.character.HumanoidRootPart.Position
+					local tntcheck
+					for i,v in pairs(damagemethods) do 
+						local item = getItem(i)
+						if item then
+							if i == "tnt" then 
+								local pos = getScaffold(LongJumpOrigin)
+								tntcheck = Vector3.new(pos.X, LongJumpOrigin.Y, pos.Z)
+								v(item, pos)
+							else
+								v(item, LongJumpOrigin)
+							end
+							break
+						end
+					end
+					local changecheck
+					LongJumpacprogressbarframe.Visible = true
+					RunLoops:BindToHeartbeat("LongJump", function(dt)
+						if entityLibrary.isAlive then 
+							if entityLibrary.character.Humanoid.Health <= 0 then 
+								LongJump.ToggleButton(false)
+								return
+							end
+							if not LongJumpOrigin then 
+								LongJumpOrigin = entityLibrary.character.HumanoidRootPart.Position
+							end
+							local newval = damagetimer ~= 0
+							if changecheck ~= newval then 
+								if newval then 
+									LongJumpacprogressbarframe2:TweenSize(UDim2.new(0, 0, 0, 20), Enum.EasingDirection.InOut, Enum.EasingStyle.Linear, 2.5, true)
+								else
+									LongJumpacprogressbarframe2:TweenSize(UDim2.new(1, 0, 0, 20), Enum.EasingDirection.InOut, Enum.EasingStyle.Linear, 0, true)
+								end
+								changecheck = newval
+							end
+							if newval then 
+								local newnum = math.max(math.floor((damagetimertick - tick()) * 10) / 10, 0)
+								if LongJumpacprogressbartext then 
+									LongJumpacprogressbartext.Text = newnum.."s"
+								end
+								if directionvec == nil then 
+									directionvec = entityLibrary.character.HumanoidRootPart.CFrame.lookVector
+								end
+								local longJumpCFrame = Vector3.new(directionvec.X, 0, directionvec.Z)
+								local newvelo = longJumpCFrame.Unit == longJumpCFrame.Unit and longJumpCFrame.Unit * (newnum > 1 and damagetimer or getSpeed()) or Vector3.zero
+								newvelo = Vector3.new(newvelo.X, 0, newvelo.Z)
+								longJumpCFrame = longJumpCFrame * 3 * dt
+								local ray = workspace:Raycast(entityLibrary.character.HumanoidRootPart.Position, longJumpCFrame, bedwarsStore.blockRaycast)
+								if ray then 
+									longJumpCFrame = Vector3.zero
+									newvelo = Vector3.zero
+								end
+
+								entityLibrary.character.HumanoidRootPart.Velocity = newvelo
+								entityLibrary.character.HumanoidRootPart.CFrame = entityLibrary.character.HumanoidRootPart.CFrame + longJumpCFrame
+							else
+								LongJumpacprogressbartext.Text = "2.5s"
+								entityLibrary.character.HumanoidRootPart.CFrame = CFrame.new(LongJumpOrigin, LongJumpOrigin + entityLibrary.character.HumanoidRootPart.CFrame.lookVector)
+								entityLibrary.character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+								if tntcheck then 
+									entityLibrary.character.HumanoidRootPart.CFrame = CFrame.new(tntcheck + entityLibrary.character.HumanoidRootPart.CFrame.lookVector, tntcheck + (entityLibrary.character.HumanoidRootPart.CFrame.lookVector * 2))
+								end
+							end
+						else
+							if LongJumpacprogressbartext then 
+								LongJumpacprogressbartext.Text = "2.5s"
+							end
+							LongJumpOrigin = nil
+							tntcheck = nil
+						end
+					end)
+				end)
+			else
+				LongJumpacprogressbarframe.Visible = false
+				RunLoops:UnbindFromHeartbeat("LongJump")
+				directionvec = nil
+				tntcheck = nil
+				LongJumpOrigin = nil
+				damagetimer = 0
+				damagetimertick = 0
+			end
+		end
+end)
